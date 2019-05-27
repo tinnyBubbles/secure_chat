0898013d1886437c5c009b4c83491ef846ecb1a1
Brandon Huff <bhuff21@gmail.com>
2019-05-27 14:21:36 -0400

    changes

--- modules/server/server.py
+++ modules/server/server.py
@@ -1,9 +1,10 @@
 import socket
 
+HOST = 127.0.0.1
 PORT = 6543
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-	s.bind((socket.gethostname(), PORT))
+	s.bind((HOST, PORT))
 	s.listen()
 	conn, addr = s.accept()
 	with conn:
