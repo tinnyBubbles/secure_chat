2c4661f6aab132e6704dc57d2ecdd6764474c363
Brandon Huff <bhuff21@gmail.com>
2019-06-03 14:44:19 -0400

    added selectors registration for client_sockObj

--- modules/new_client_server/server_selector.py
+++ modules/new_client_server/server_selector.py
@@ -11,16 +11,36 @@ read_sockets = []
 error_sockets = []
 clients = {}
 
+
 def receive_message(socketObj):
-    message_header = int(socketObj.recv(HEADER_LENGTH).decode("UTF-8"))
-    message_data = socketObj.recv(message_header).decode("UTF-8")
-    message = (message_header, message_data)
-    return message(message_header, message_data)
+    usrID = socketObj.gethostbyname()
+    message_header = socketObj.recv(HEADER_LENGTH)
+    message_length = int(message_header.decode("UTF-8").strip())
+    message_data = socketObj.recv(message_length)
+    message_text = message_data.decode("UTF-8")
+    message = {usrID: (message_length, message_text)}
+    return message
 
 
-def send_message(socketObj, message_header, message_data):
+def send_message(socketObj, message_length, message_text):
     socketObj.sendall(message_header.encode("UTF-8") + message_data.encode("UTF-8"))
 
 
 def record_conv(clients):
     pass
+
+
+sel = selectors.DefaultSelector()
+server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_sock.bind(IP, PORT)
+server_sock.listen()
+
+while len(clients) < 2:
+    client_sock, client_addr = server_sock.accept()
+    clients.update({client_socket: client_addr})
+
+    sel.register(client_sock, selectors.EVENT_READ, receive_message)
+
+while True:
+
