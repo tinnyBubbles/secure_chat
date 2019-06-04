be8bc98cf13385d32fbb82a8f70dde9b4a5c7d5c
Brandon Huff <bhuff21@gmail.com>
2019-06-04 16:44:29 -0400

    cleaned script naming

--- modules/new_client_server/server_selector.py
+++ modules/new_client_server/server_selector.py
@@ -6,22 +6,22 @@ HEADER_LENGTH = 10
 IP_ADDR = "127.0.0.1"
 PORT = 1234
 
-Client_info = namedtuple('Client_info', ['clientAddr', 'msgLength', 'msgData'])
+ClientKey = namedtuple('ClientKey', ['msgLength', 'msgData'])
 Clients = {}
 
 def receive_message(socketObj):
-    usrID = socketObj.gethostbyname()
     message_header = socketObj.recv(HEADER_LENGTH)
     message_length = int(message_header.decode("UTF-8").strip())
+
     message_data = socketObj.recv(message_length)
-    message_plText = message_data.decode("UTF-8")
-    clientKey = Client_info(usrID, message_length, message_data)
-    #message = (message_length, message_text)
-    return clientKey
+
+    key = ClientKey(message_length, message_data)
+
+    return key
 
 
-def send_message(socketObj, message_length, message_text):
-    socketObj.sendall(message_header.encode("UTF-8") + message_data.encode("UTF-8"))
+def send_message(socketObj, msgHeader, msgData):
+    socketObj.sendall(msgHeader + message_data)
 
 
 def record_conv(clients):
@@ -32,24 +32,24 @@ sel = selectors.DefaultSelector()
 server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server_sock.bind((IP_ADDR, PORT))
-server_sock.listen()
+server_sock.listen(2)
 
 while len(Clients) < 2:
-    client_sock, client_addr = server_sock.accept()
-    clients.update({client_socket: client_addr})
+    conn, addr = server_sock.accept()
+    Clients.update({conn: addr})
 
-    sel.register(client_sock, selectors.EVENT_READ, receive_message)
+    sel.register(conn, selectors.EVENT_READ, receive_message)
 
 while True:
-    ready_sockets = sel.select()
-    for key, event in ready_sockets:
+    read_sockets = sel.select()
+    for key, event in read_sockets:
         read_msg = key.data
         sock = key.fileobj
         clientKey = read_msg(sock)
-        for client in clients:
+        for client in Clients:
             if client == sock:
                 continue
             else:
-                send_message(client, clientKey.msgLength, clientKey.msgData)  
+                send_message(client, clientKey.message_length, clientKey.message_data)  
 
 
