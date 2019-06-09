940e9dd0f7f29954fbbc5f433bce0fec76654951
Brandon Huff <bhuff21@gmail.com>
2019-06-08 23:09:04 -0400

    Constructed basic client

--- modules/new_client_server/client.py
+++ modules/new_client_server/client.py
@@ -35,27 +35,64 @@ Loop
 import socket
 import encrypt as e
 
+IP = '127.0.0.1'
+PORT = 1234
+
+
 class client:
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
-    def send(self):
-        pass
+    def send(self, message):
+        try:
+            self.sock.send(message.encode('UTF-8'))
+            return 1
+        except Exception:
+            return Exception
 
+    def receive(self):
+        try:
+            message = self.sock.recv(2000)
+            return message.decode('UTF-8') 
+        except Exception:
+            return Exception
 
-    def read(self):
-        pass
-
-
-    def connect(self):
-        pass
-
+    def connect(self, ip, port):
+        try:
+            self.sock.connect((ip, port))
+            return 1
+        except Exception:
+            return Exception
 
     def encrypt(self):
         pass
 
-
     def decrypt(self):
         pass
+
+client = client()
+
+connection_status = client.connect((IP, PORT))
+
+if status_msg == 1:
+    print("Connected ...")
+else:
+    print(status_msg)
+    sys.exit()
+    
+while True:
+
+    message = input("-->")
+
+    send_status = client.send(message)
+
+    if send_status == 1:
+        print("Message sent")
+    else:
+        print(send_status)
+        sys.exit()
+
+
+
 
