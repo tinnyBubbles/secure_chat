7e6e6fe0d6893274d15a1995b5b4ce7b76d6554f
Brandon Huff <bhuff21@gmail.com>
2019-06-17 10:35:04 -0400

    Began building protoypes

--- modules/chat2.py
+++ modules/chat2.py
@@ -1,16 +1,47 @@
 import socket, sys
 from multiprocessing import Process
+from abc import ABC
 
 
-class Chat:
+class Abstract_Chat(ABC):
+    @abstractmethod
+    def send():
+        pass
+
+
+    @abstractmethod
+    def receive():
+        pass
+
+
     pass
 
 
-class Client:
+class MainFactory:
+    @staticmethod
+    def build_client():
+        pass
+
+    @staticmethod
+    def build_server():
+        pass
+
+    @staticmethod
+    def build_logger():
+        pass
+
+    @staticmethod
+    def build_cipher():
+        pass
+
     pass
 
 
-class Server:
+class Client(Abstract_Chat):
+    pass
+
+
+class Server(Abstract_Chat):
     pass
 
 
