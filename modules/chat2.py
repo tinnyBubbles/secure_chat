188e9b597ae5c268080bf8a8057028f315d5cf5a
Brandon Huff <bhuff21@gmail.com>
2019-06-16 23:57:56 -0400

    initial commit

---
+++ modules/chat2.py
@@ -0,0 +1,35 @@
+import socket, sys
+from multiprocessing import Process
+
+
+class Chat:
+    pass
+
+
+class Client:
+    pass
+
+
+class Server:
+    pass
+
+
+class Logger:
+    pass
+
+
+class Monitor:
+    pass
+
+
+class UI:
+    pass
+
+
+class Cipher:
+    pass
+
+
+def main():
+    pass
+
