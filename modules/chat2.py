cfdbc3a592654cfb128c2f6bc37e79b12e34ae42
Brandon Huff <bhuff21@gmail.com>
2019-06-26 16:17:42 -0400

    built client, server, and abstract_chat classes

--- modules/chat2.py
+++ modules/chat2.py
@@ -4,46 +4,49 @@ from abc import ABC
 
 
 class Abstract_Chat(ABC):
-    @abstractmethod
-    def send():
-        pass
-
-
-    @abstractmethod
-    def receive():
-        pass
-
-
-    pass
-
-
-class MainFactory:
     @staticmethod
-    def build_client():
-        pass
+    def send(sock, msg):
+        sock.send(msg.encode('UTF-8')
+
 
     @staticmethod
-    def build_server():
-        pass
-
-    @staticmethod
-    def build_logger():
-        pass
-
-    @staticmethod
-    def build_cipher():
-        pass
-
-    pass
-
+    def receive(sock):
+        msg = sock.recv(2000)
+        return msg.decode('UTF-8')
 
 class Client(Abstract_Chat):
-    pass
+    def __init__(self, server_ip, server_port):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                  
+        self.server_ip = server_ip
+        self.server_port = server_port
+        self.sock.settimeout(20)
 
+    def connect(self):
+        try:
+            self.sock.connect((self.server_ip, self.server_port))
+            print("You are now connected ...")
+            return True, self.sock
+        except:
+            print("Could not connect ...")
+            return False
+        
 
 class Server(Abstract_Chat):
-    pass
+    def __init__(self, sock, host_ip, host_port)
+        self.sock = sock
+        self.host_ip = host_ip
+        self.host_port = host_port
+    
 
+    def accept_conn(self):
+        print("Listening ...")
+        self.sock.bind((self.host_ip, self.host_port))
+        self.sock.listen()
+
+        return self.sock.accept()
+        print("Accepted a connection")
+        
 
 class Logger:
     pass
@@ -62,5 +65,17 @@ class Cipher:
 
 
 def main():
-    pass
+    chat_client = Client(s, SERVER_IP, SERVER_PORT)
+
+    is_connected, s = chat_client.connect()
+    
+    if is_connected == True:
+        #send and receive messages on seperate processes
+
+    elif is_connected == False:
+        chat_server = Server(s, HOST_IP, HOST_PORT)
+        conn, addr = chat_server.accept_conn()
+    else:
+        print("Something went wrong. Exiting")
+        sys.exit()
 
